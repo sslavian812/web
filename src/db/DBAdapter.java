fc1e188f02301541f8a2d44981997cf081814f89
hotkey <hotkeytlt@gmail.com>
2014-12-19 17:22:44 +0300

    Replace while with if when it meant if. Removed empty Javadoc since every instance was an IDE warning.

--- src/db/DBAdapter.java
+++ src/db/DBAdapter.java
@@ -103,8 +103,6 @@ public class DBAdapter {
 
     /**
      * closes the database
-     *
-     * @throws SQLException
      */
     public static boolean close() {
         try {
@@ -170,7 +168,7 @@ public class DBAdapter {
         s.setInt(1, user_id);
         resultSet = s.getResultSet();
 
-        while (resultSet.next()) {
+        if (resultSet.next()) {
             long time = resultSet.getLong("expires");
             String value = resultSet.getString("token");
             if(time + 60 < curTime)
@@ -187,8 +185,6 @@ public class DBAdapter {
     /**
      * add a user to database, creates default "history" list.
      *
-     * @param login
-     * @param password
      * @throws SQLException
      */
     public static long addUser(String login, String password) throws SQLException {
@@ -211,8 +207,6 @@ public class DBAdapter {
      * Returns user given his login.
      * Unsafe method. be careful.
      *
-     * @param login
-     * @return
      * @throws SQLException
      */
     public static User getUserByLogin(String login) throws SQLException {
@@ -233,9 +227,6 @@ public class DBAdapter {
     /**
      * returns user from table iff login and password match
      *
-     * @param login
-     * @param password
-     * @throws ClassNotFoundException
      * @throws SQLException
      */
     public static User getUserByCredentials(String login, String password) throws SQLException {
@@ -284,8 +275,6 @@ public class DBAdapter {
     /**
      * adds list to usr by his id.
      *
-     * @param user_id
-     * @param name
      * @throws SQLException
      */
     public static void addList(long user_id, String name) throws SQLException {
@@ -298,9 +287,6 @@ public class DBAdapter {
     /**
      * returns id of list by it's name owned by the user or -1 if doesn't exist
      *
-     * @param user_id
-     * @param name
-     * @return
      * @throws SQLException
      */
     public static int getListId(int user_id, String name) throws SQLException {
@@ -317,9 +303,6 @@ public class DBAdapter {
     /**
      * returns db.WordsList object or null if now exists
      *
-     * @param user_id
-     * @param name
-     * @return
      * @throws SQLException
      */
     public static WordsList getList(int user_id, String name) throws SQLException {
@@ -328,7 +311,7 @@ public class DBAdapter {
         s.setString(2, name);
         resultSet = s.executeQuery();
 
-        while (resultSet.next()) {
+        if (resultSet.next()) {
             int id = resultSet.getInt("id");
             int uid = resultSet.getInt("user_id");
             String nm = resultSet.getString("name");
@@ -342,7 +325,6 @@ public class DBAdapter {
      * deletes specified list and all the bindings between it and words.
      * words itself are leaving in the database even when no list contains it.
      *
-     * @param list_id
      * @throws SQLException
      */
     public static void deleteList(int list_id) throws SQLException {
@@ -357,10 +339,6 @@ public class DBAdapter {
     /**
      * inserts a word to words and associated it with specified list
      *
-     * @param list_id
-     * @param word
-     * @param translation
-     * @param article_json
      * @throws SQLException
      */
     public static void addWord(int list_id, String word, String translation, String article_json) throws SQLException {
@@ -383,8 +361,6 @@ public class DBAdapter {
     /**
      * inserts a word to words and associated it with specified list
      *
-     * @param list_id
-     * @param word_id
      * @throws SQLException
      */
     public static void addWordToList(int list_id, int word_id) throws SQLException {
@@ -398,8 +374,6 @@ public class DBAdapter {
     /**
      * return db.Word object, or null if it not exists
      *
-     * @param word
-     * @return
      * @throws SQLException
      */
     public static Word getWord(String word) throws SQLException {
@@ -421,8 +395,6 @@ public class DBAdapter {
     /**
      * returns id of word if exists. otherwise throws NullPointerException
      *
-     * @param word
-     * @return
      * @throws SQLException
      */
     public static int getWordID(String word) throws SQLException {
@@ -433,7 +405,7 @@ public class DBAdapter {
     /**
      * deletes the words from database and destroys all the binding it to every list.
      * Suppose, we do need this method at all.
-     * @param word_id
+     *
      * @throws SQLException
      */
     public static void deleteWord(int word_id) throws SQLException {
@@ -445,8 +417,6 @@ public class DBAdapter {
     /**
      * deletes the words from database and destroys all the binding it to every list
      *
-     * @param word_id
-     * @param list_id
      * @throws SQLException
      */
     public static void deleteWordFromList(int word_id, int list_id) throws SQLException
@@ -461,8 +431,6 @@ public class DBAdapter {
     /**
      * returns all the words in the specified list
      *
-     * @param listId
-     * @return
      * @throws SQLException
      */
     public static List<Word> getWordsFromList(int listId) throws SQLException {
@@ -489,8 +457,6 @@ public class DBAdapter {
     /**
      * return all the Lists owned by user
      *
-     * @param userId
-     * @return
      * @throws SQLException
      */
     public static List<WordsList> getListsByUserId(int userId) throws SQLException {
@@ -537,8 +503,6 @@ public class DBAdapter {
     /**
      * associates a cookie-value with specified user
      *
-     * @param user_id
-     * @param value
      * @throws SQLException
      */
     public static void addCookie(long user_id, String value) throws SQLException {
@@ -552,7 +516,6 @@ public class DBAdapter {
     /**
      * deletes a cookie associated with specifies user.
      *
-     * @param user_id
      * @throws SQLException
      */
     public static void deleteCookie(int user_id) throws SQLException {
@@ -564,8 +527,6 @@ public class DBAdapter {
     /**
      * return all valid cookies by user id in a list
      *
-     * @param user_id
-     * @return
      * @throws SQLException
      */
     public static List<String> getCookieByUserID(int user_id) throws SQLException {
